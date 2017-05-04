d2f80a1dc40020a2e758a9caaf23a9a4c606023d
lufubinGit <lufubin@147hotmail.com>
2017-05-04 17:26:59 +0800

    Initial Commit

---
+++ runtimeTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  runtimeTest
+//
+//  Created by JD on 2017/5/4.
+//  Copyright © 2017年 JD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
